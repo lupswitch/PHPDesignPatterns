daa286490d9c318c6da4d3d3c98a7d683308c290
Varpikhovsky Svyatoslav <varpihovsky@gmail.com>
2017-02-12 18:51:20 +0200

    Add iterator as example

--- Behavioral/Iterator/Iteratable.php
+++ Behavioral/Iterator/Iteratable.php
@@ -1,47 +1,70 @@
 <?php
-
 namespace PHPDesignPatterns\Behavioral\Iterator;
 
-
-class Iteratable  implements   \Iterator
+/**
+ * Class Iteratable
+ * @package PHPDesignPatterns\Behavioral\Iterator
+ */
+class Iteratable implements \Iterator
 {
 
-  protected  $position = 0;
+    /**
+     * Current possition
+     * @var int
+     */
+    protected $position = 0;
 
-  protected  $data = [
-    'Item1',
-    'Item2',
-    'Item3',
-  ];
 
-  public  function  __construct()
-  {
-    $this->position = 0;
+    /**
+     * Default values
+     * @var array
+     */
+    protected $data = [
+        'Item1',
+        'Item2',
+        'Item3',
+    ];
 
-  }
 
-  public function rewind ()
-  {
-    $this->position = 0;
-  }
+    /**
+     * Iteratable constructor.
+     */
+    public function __construct()
+    {
+        $this->position = 0;
 
-  public function current()
-  {
-    return $this->data[$this->position];
-  }
+    }
 
-  public function  key ()
-  {
-    return $this->position;
-  }
 
-  public function next ()
-  {
-    ++$this->position;
-  }
+    /**
+     *
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
 
-  public function valid ()
-  {
-    return isset($this->data[$this->position]);
-  }
+    public function current()
+    {
+        return $this->data[$this->position];
+    }
+
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        ++$this->position;
+    }
+
+
+    /**
+     * @return bool             
+     */
+    public function valid()
+    {
+        return isset($this->data[$this->position]);
+    }
 }
