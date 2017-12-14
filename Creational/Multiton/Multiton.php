03b2d57253a6cb71ebb3445c453cb5b10ebc7d31
Varpikhovsky Svyatoslav <varpihovsky@gmail.com>
2017-12-14 09:19:40 +0200

    Add Creational/Multiton

---
+++ Creational/Multiton/Multiton.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Script
+ * Date: 26.11.2017
+ * Time: 17:08
+ */
+
+namespace PHPDesignPatterns\Creational\Multiton;
+
+
+class Multiton
+{
+
+}
---
+++ Creational/Multiton/Tests/MultitonTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: Script
+ * Date: 26.11.2017
+ * Time: 17:11
+ */
+
+namespace PHPDesignPatterns\Creational\Multiton\Tests;
+
+
+class MultitonTest extends \PHPUnit\Framework\TestCase
+{
+    public function test()
+    {
+        $this->assertEquals(true, true);
+    }
+}
