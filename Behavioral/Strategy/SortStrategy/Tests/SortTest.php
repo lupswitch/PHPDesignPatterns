cc089f41c5b1c64ff66905c5f03489b0657d5723
Varpikhovsky Svyatoslav <varpihovsky@gmail.com>
2018-11-07 06:36:04 +0200

    fix test

--- Behavioral/Strategy/SortStrategy/Tests/SortTest.php
+++ Behavioral/Strategy/SortStrategy/Tests/SortTest.php
@@ -8,6 +8,8 @@
 namespace PHPDesignPatterns\Behavioral\Strategy\SortStrategy\Tests;
 
 
+use PHPDesignPatterns\Behavioral\Strategy\SortStrategy\BubbleSortStrategy;
+use PHPDesignPatterns\Behavioral\Strategy\SortStrategy\QuickSortStrategy;
 use PHPDesignPatterns\Behavioral\Strategy\SortStrategy\Sorter;
 use PHPUnit\Framework\TestCase;
 
@@ -18,14 +20,11 @@ class SortTest extends TestCase
         $dataset = [1, 5, 4, 3, 2, 8];
 
         $sorter = new Sorter(new BubbleSortStrategy());
-        $result = $sorter->sort($dataset); // Output : Пузырьковая сортировка
-
+        $result = $sorter->sort($dataset);
         $this->assertEquals('Bubble', $result['sort']);
 
         $sorter = new Sorter(new QuickSortStrategy());
-
-        $result = $sorter->sort($dataset); // Output : Быстрая сортировка
-
+        $result = $sorter->sort($dataset);
         $this->assertEquals('Quick', $result['sort']);
     }
 }
