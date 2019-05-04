5bfebf4d27b6c6f5ba62a58b1b5262a1ea35118d
Varpikhovsky Svyatoslav <varpihovsky@gmail.com>
2019-05-04 13:36:53 +0300

    Fix test

--- Creational/FactoryMethod/CreateProduct/Tests/FactoryMethodTest.php
+++ Creational/FactoryMethod/CreateProduct/Tests/FactoryMethodTest.php
@@ -9,21 +9,27 @@
 namespace PHPDesignPatterns\Creational\FactoryMethod\CreateProduct\Tests;
 
 use PHPDesignPatterns\Creational\FactoryMethod\CreateProduct\ConcreteCreator1;
+use PHPDesignPatterns\Creational\FactoryMethod\CreateProduct\ConcreteCreator2;
 use PHPUnit\Framework\TestCase;
 
 class FactoryMethodTest extends TestCase
 {
     /**
-     *
+     * Test product 1
      */
     public function testProduct1() {
         $creator = new ConcreteCreator1();
-        $creator->someOperation();
+        $r = $creator->someOperation();
+
+        $this->assertEquals('Creator: The same creator\'s code has just worked with {Result of the ConcreteProduct1}', $r);
     }
 
-
+    /**
+     * Test product 2
+     */
     public function testProduct2() {
         $creator = new ConcreteCreator2();
-        $creator->someOperation();
+        $r =$creator->someOperation();
+        $this->assertEquals('Creator: The same creator\'s code has just worked with {Result of the ConcreteProduct2}', $r);
     }
 }
